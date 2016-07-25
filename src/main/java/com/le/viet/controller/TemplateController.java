bf8915674b08af3215e4ffa2bb0ae429ed60d3b5
VLE <vietle@viets-mac-mini.attlocal.net>
2016-07-24 21:38:34 -0400

    add hashing sha256

--- src/main/java/com/le/viet/controller/TemplateController.java
+++ src/main/java/com/le/viet/controller/TemplateController.java
@@ -1,8 +1,10 @@
 package com.le.viet.controller;
 
+import java.security.MessageDigest;
 import java.util.Date;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,26 @@ public class TemplateController {
 		Date now = new Date();
 		return now.getTime();
 	}
-	
+
+	@RequestMapping(value = "/hash", method = RequestMethod.POST)
+	public String hashPassword(@RequestBody String password){
+		StringBuffer sb = new StringBuffer();
+		try{
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			md.update(password.getBytes());
+
+			byte byteData[] = md.digest();
+
+			for (int i = 0; i < byteData.length; i++) {
+				sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			System.out.println("Hex format : " + sb.toString());
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+		return sb.toString();
+	}
+
 	@RequestMapping(value = "/ping", method = RequestMethod.GET)
 	public String ping(){
 		return "pong";
--- src/main/java/com/le/viet/designpattern/observer/ObserverClient.java
+++ src/main/java/com/le/viet/designpattern/observer/ObserverClient.java
@@ -4,10 +4,10 @@ package com.le.viet.designpattern.observer;
  * Created by vietle on 7/14/16.
  */
 public class ObserverClient {
-    public static void main(String[] args){
+    /*public static void main(String[] args){
         System.out.println("I'M AN OBSERVER CLIENT");
         CardData cardData = new CardData();
         CardDataObserver cardDataObserver = new CardDataObserver(cardData);
         cardDataObserver.showCardData();
-    }
+    }*/
 }
